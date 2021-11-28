d2333f16d8496a0cf931b3bdca37a1eb7c0760ba
Alex-EmSiKay <alexandercmckinlay@gmail.com>
2021-11-28 20:59:41 +1100

    set up gitignore

--- new-scripts.js
+++ new-scripts.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const input_field = element.querySelector(".input");
         const render_field = element.querySelector(".render");
         input_field.addEventListener('input', () => {
-            render_field.innerText = input_field.innerText
+            render_field.innerText = input_field.innerText;
             renderMathInElement(render_field, {
                 // customised options
                 // • auto-render specific keys, e.g.:
@@ -14,6 +14,9 @@ document.addEventListener('DOMContentLoaded', () => {
                 // • rendering keys, e.g.:
                 throwOnError: true
             });
+        })
+        input_field.addEventListener('blur', () => {
+            input_field.innerText = input_field.innerText
         })
     })
 })
