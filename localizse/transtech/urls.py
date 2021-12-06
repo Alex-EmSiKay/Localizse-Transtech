23bffdb57b8daf9b9ee09caa9ea11b99011b52eb
Alex-EmSiKay <alexandercmckinlay@gmail.com>
2021-12-06 23:36:24 +1100

    set up site structure

--- localizse/transtech/urls.py
+++ localizse/transtech/urls.py
@@ -1,4 +1,14 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("", views.index, name="index")]
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("messages", views.messages, name="messages"),
+    path("register", views.register, name="register"),
+    path("work", views.work, name="work"),
+    path("work/<str:type>", views.work, name="work_by_type"),
+    path("account", views.account, name="account"),
+    path("finance", views.finance, name="finance"),
+]
--- localizse/transtech/views.py
+++ localizse/transtech/views.py
@@ -1,5 +1,99 @@
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.db import IntegrityError
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+from .models import User
+
+
+@login_required
+def account(request):
+    return render(request, "transtech/account.html", {"user_email": request.user.email})
 
 
 def index(request):
-    return render(request, "transtech/index.html")
+    if request.user.is_authenticated:
+        return render(request, "transtech/index.html")
+    else:
+        return HttpResponseRedirect(reverse("login"))
+
+
+@login_required
+def finance(request):
+    pass
+
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        email = request.POST["email"]
+        password = request.POST["password"]
+        user = authenticate(request, username=email, password=password)
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "transtech/login.html")
+    else:
+        if request.user.is_authenticated:
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "transtech/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("login"))
+
+
+@login_required
+def messages(request):
+    return render(request, "transtech/messages.html")
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["email"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(
+                request, "transtech/register.html", {"message": "Passwords must match."}
+            )
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(
+                request,
+                "transtech/register.html",
+                {"message": "email already registered."},
+            )
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        if request.user.is_authenticated:
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "transtech/register.html")
+
+
+@login_required
+def work(request, type):
+    if not type:
+        pass
+    elif type in [
+        "create",
+        "review",
+        "audit",
+    ]:
+        return render(request, f"transtech/{type}.html")
